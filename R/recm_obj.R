81ee05e8ef9bb9b6c0a928cadfa2054de777b4ea
David L Gibbs <gibbsdavidl@gmail.com>
2022-01-24 14:41:42 -0800

    sigpairs now working

--- R/recm_obj.R
+++ R/recm_obj.R
@@ -6,6 +6,16 @@ library(data.table)
 
 source('R/enbl_obj.R')
 
+data_bin_4 <- function(x) {
+  p = quantile(x)
+  if (any(table(p) > 1)) {
+    # then we have duplicate levels
+    p <- unique(p)
+  }
+  as.numeric(cut(x, breaks=p, include.lowest = T))
+}
+
+
 #' Recm  Robust ensemble classifier machine (Recm)
 #' 
 #' An object that holds data and an ensemble of classifiers
@@ -84,42 +94,54 @@ Recm <- R6Class("Recm",
                     read_data = function(file_name, sep, header) {
                       self$file_name <- file_name
                       self$data <- data.table::fread(file=file_name, sep=sep, header=T)
+                      colnames(self$data) <- gsub(" ", "_", colnames(self$data))
                     },
-                    
+                  
                     
                     # data engineering
                     data_eng = function() {
-                      print(paste0("self mode: ", self$data_mode))
                       
+                      if (!all(self$data_mode %in% c('original','quartiles','pairs','ranks','sigpairs'))) {
+                        print("ERROR:  please choose a valid collection of data modes: ")
+                        print('original  quartiles  pairs  ranks  sigpairs')
+                        stop('data_mode, wrong value')
+                      }
+
                       rankdat <- NULL
                       pairdat <- NULL
                       newdat <- data.table()
                       
-                      if ('noproc' %in% self$data_mode) {
-                        print("*** NO DATA PROCESSING ***")
+                      if ('original' %in% self$data_mode) {
+                        print("*** Keeping Orig. Data ***")
                         # do not process data
-                        return()
+                        newdat <- cbind(newdat, self$data)
                       }
                       
-                      if ('rank' %in% self$data_mode) {
+                      if ('quartiles' %in% self$data_mode) {
+                        print("*** BUILDING QUARTILES ***")
                         cols <- colnames(ann$data)
-                        rankdat <- ann$data[ , (cols) := lapply(.SD, "rank"), .SDcols = cols]
-                        newdat <- cbind(newdat, rankdat)
+                        quartdat <- ann$data[ , (cols) := lapply(.SD, "data_bin_4"), .SDcols = cols]
+                        colnames(quartdat) <- sapply(cols, function(a) paste0(a,'_quartile',collapse = ''))
+                        newdat <- cbind(newdat, quartdat)
                       } 
+                      
+                      if ('ranks' %in% self$data_mode) {
+                        print("*** RANKING DATA ***")
+                        cols <- colnames(ann$data)
+                        rankdat <- ann$data[ , (cols) := lapply(.SD, "rank", ties.method="min"), .SDcols = cols]
+                        colnames(rankdat) <- sapply(cols, function(a) paste0(a,'_ranked',collapse = ''))
+                        newdat <- cbind(newdat, rankdat)
+                      }
                       
                       if ('pairs' %in% self$data_mode) {
                         print("*** PROCESSING DATA: PAIRS ***")
                         # if mode includes 'pairs' then we need to make var-pairs
                         newcol_names <- c()
                         newcol_dat <- list()
-                        if (is.null(rankdat)) {
-                          cols <- colnames(ann$data)
-                          # we are going to compare ranked data, to make it fair comparisons.
-                          rankdat = ann$data[ , (cols) := lapply(.SD, "rank"), .SDcols = cols]
-                        }
+                        cols <- colnames(ann$data)
                         for (ci in 1:(length(cols)-1)) {
                           for (cj in (ci+1):length(cols)) {
-                            res0 <- as.numeric(rankdat[,.SD,.SDcols=ci] > rankdat[,.SD,.SDcols=cj])
+                            res0 <- as.numeric(self$data[,.SD,.SDcols=ci] > self$data[,.SD,.SDcols=cj])
                             this_new_col <- paste0(cols[ci],'_X_', cols[cj])
                             newcol_names <- c(newcol_names, this_new_col)
                             newcol_dat[[this_new_col]] <- res0
@@ -127,6 +149,39 @@ Recm <- R6Class("Recm",
                         }
                         pairdat <- data.table(data.frame(newcol_dat))
                         newdat <- cbind(newdat, pairdat)
+                      }
+                      
+                      if ('sigpairs' %in% self$data_mode) {
+                        print("*** COMPUTING SIGNATURE PAIRS ***")
+                        newcol_names <- c()
+                        newcol_dat <- list()
+                        sig_names <- names(ann$signatures)
+                        for (ci in 1:(length(sig_names)-1)) {
+                          for (cj in (ci+1):length(sig_names)) {
+                            # Now we are looking at a pair of signatures
+                            sn1 <- sig_names[ci]
+                            sn2 <- sig_names[cj]
+                            s1 <- gsub(' ', '_', self$signatures[[sn1]])
+                            s2 <- gsub(' ', '_', self$signatures[[sn2]])
+                            sig_pair_temp <- list()
+                            # for each pair, look at each pair of genes, record gt or lt
+                            for (sx1 in s1) {
+                              for (sx2 in s2) {
+                                res0 <- as.numeric(self$data[,.SD,.SDcols=sx1] > self$data[,.SD,.SDcols=sx2])
+                                sig_pair_temp[[paste0(sx1,sx2,collapse = '_X_')]] <- res0
+                              }
+                            }
+                            # first we make a df
+                            resdf <- data.frame(sig_pair_temp)
+                            findf <- apply(resdf, 1, sum, na.rm=T) / ncol(resdf)
+                            #
+                            this_new_col <- paste0(sig_names[ci],'_X_', sig_names[cj])
+                            newcol_names <- c(newcol_names, this_new_col)
+                            newcol_dat[[this_new_col]] <- findf
+                          }
+                        }
+                        sigpairs_dat <- data.table(data.frame(newcol_dat))
+                        newdat <- cbind(newdat, sigpairs_dat)
                       }
 
                       # save the final processed data table.
@@ -147,6 +202,10 @@ Recm <- R6Class("Recm",
                       self$data_mode <- data_mode
                       self$signatures <- signatures
                       self$label <- sapply(self$data[[label_name]], as.character)
+                      # fix any spaces in the column names
+                      label_name <- gsub(' ', '_', label_name)
+                      drop_list <- gsub(' ', '_', drop_list)
+                      # then apply with those names
                       self$data[, (label_name):=NULL]
                       self$data[, (drop_list):=NULL]
                       # DATA ENGINEERING
--- test/example_auto_prediction.R
+++ test/example_auto_prediction.R
@@ -6,21 +6,21 @@ source('R/recm_obj.R')
 
 ann <- Recm$new("Ann")
 
-sigs = list(Sig1=c('Uniformity.of.Cell.Shape','Uniformity.of.Cell.Shape', 'Marginal.Adhesion'), 
-            Sig2=c('Bare.Nuclei', 'Normal.Nucleoli', 'Single.Epithelial.Cell.Size'),
-            Sig3=c('Bland.Chromatin', 'Mitoses'))
+sigs = list(Sig1=c('Uniformity of Cell Shape','Uniformity of Cell Size', 'Marginal Adhesion'), 
+            Sig2=c('Bare Nuclei', 'Normal Nucleoli', 'Single Epithelial Cell Size'),
+            Sig3=c('Bland Chromatin', 'Mitoses'))
 
 ann$autopred(data_file='data/Breast Cancer Prediction.csv',
              sep=',',
              label_name='Class',
              drop_list = c('Sample code number'),
              data_split=0.60,
-             data_mode='pairs', #c('rank', 'pairs'), #, 'sigpairs'), #c('noproc'),
+             data_mode=c('sigpairs'), # 'quartiles', c('original', 'ranks', 'pairs'), # 'sigpairs'
              signatures=sigs,
              size=8,
              max_depth=6,
-             eta=0.5,
-             nrounds=7,
+             eta=0.1,
+             nrounds=5,
              nthreads=4,
              objective="binary:logistic",
              train_perc=0.5,
