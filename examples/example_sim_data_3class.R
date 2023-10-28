7dd20dd15be08b2db13e15d689973dd3fe0b3cd2
David L Gibbs <gibbsdavidl@gmail.com>
2023-10-27 17:10:59 -0700

    update to examples

---
+++ examples/example_sim_data_3class.R
@@ -0,0 +1,55 @@
+
+# Example using the autocv.
+
+# This data was designed so that it can only (?) be predicted with paired 
+# features after moving to a test set that is generated on an imaginary
+# but very different platform with different levels of measures.
+
+library(devtools)
+
+tmp_lib <- "E:/Work/Code/tmp_lib"
+dir.create(tmp_lib)
+
+devtools::install_local("E:/Work/Code/robencla/", lib = tmp_lib)
+
+## restart R
+
+## explicitly load the affected packages from the temporary library
+tmp_lib <- "E:/Work/Code/tmp_lib"
+library(robencla, lib.loc = tmp_lib)
+
+
+# our robust ensemble classifier machine
+mod <- Robencla$new("Test1")
+
+features <- c('X1','X2','X3','X4','X5','X6','X7','X8','X9','X10','X11','X12')
+
+# xgboost parameters
+params <- list(
+  max_depth=6,    # "height" of the tree, 6 is actually default. I think about 12 seems better.  (xgboost parameter)
+  eta=0.2,        # this is the learning rate. smaller values slow it down, more conservative   (xgboost parameter)
+  nrounds=48,     # number of rounds of training, lower numbers less overfitting (potentially)  (xgboost parameter)
+  early_stopping_rounds=2, # number of rounds without improvment stops the training (xgboost early_stopping_rounds)
+  nthreads=4,     # parallel threads
+  gamma=0.5,      # Minimum loss reduction required to again partition a leaf node. higher number ~ more conservative (xgboost parameter)
+  lambda=1.5,     # L2 regularization term on weights, higher number ~ more conservative (xgboost parameter)
+  alpha=0.5,      # L1 regularization term on weights. higher number ~ more conservative (xgboost parameter)
+  size=11,        # Size of the ensemble, per binary prediction 
+  train_perc=0.8, # The percentage of data used to train each ensemble member.
+  combine_function='median',  # How the ensemble should be combined. Only median currently.
+  verbose=0)
+
+# this will train the classifier and test it on a small 20% split
+mod$autocv(data_file='examples/data/sim_data_3classes_train.csv',
+            label_name='label',
+            sample_id=NULL,
+            cv_rounds=5,
+            data_mode=c('allpairs'), #'allpairs',  'pairs', 'sigpairs' 'quartiles', 'original', 'ranks',\
+            signatures=NULL,
+            pair_list=features,
+            params=params)
+
+# metrics on the test split
+print("Across splits")
+mod$classification_metrics() %>% print()
+
---
+++ examples/example_train_and_predict.R
@@ -0,0 +1,63 @@
+
+
+
+# Example using the autopred (auto-prediction) function.
+
+library(robencla)
+
+mod <- Robencla$new("Test3")
+
+# list of signatures to compare
+sigs = list(Sig1=c('Uniformity of Cell Shape','Uniformity of Cell Size', 'Marginal Adhesion'), 
+            Sig2=c('Bare Nuclei', 'Normal Nucleoli', 'Single Epithelial Cell Size'),
+            Sig3=c('Bland Chromatin', 'Mitoses'))
+
+# only pair these features
+my_pairs <- c('Clump Thickness','Uniformity of Cell Size','Uniformity of Cell Shape','Marginal Adhesion')
+
+# xgboost parameters
+params <- list(
+  max_depth=6,    # "height" of the tree, 6 is actually default. I think about 12 seems better.  (xgboost parameter)
+  eta=0.3,        # this is the learning rate. smaller values slow it down, more conservative   (xgboost parameter)
+  nrounds=48,     # number of rounds of training, lower numbers less overfitting (potentially)  (xgboost parameter)
+  early_stopping_rounds=2, # number of rounds without improvment stops the training (xgboost early_stopping_rounds)
+  nthreads=4,     # parallel threads
+  gamma=0.0,      # Minimum loss reduction required to again partition a leaf node. higher number ~ more conservative (xgboost parameter)
+  lambda=1.0,     # L2 regularization term on weights, higher number ~ more conservative (xgboost parameter)
+  alpha=0.0,      # L1 regularization term on weights. higher number ~ more conservative (xgboost parameter)
+  size=11,        # Size of the ensemble, per binary prediction 
+  train_perc=0.8, # The percentage of data used to train each ensemble member.
+  combine_function='median',  # How the ensemble should be combined. Only median currently.
+  verbose=0)
+
+
+# split the data, train and test
+mod$train(data_file='examples/data/bcp_train_data.csv',
+              label_name='Class',
+              sample_id = 'Sample code number',
+              data_mode=c('allpairs', 'sigpairs'), # allpairs, pairs, sigpairs,quartiles,tertiles,binary,ranks,original
+              pair_list=my_pairs,
+              signatures=sigs,
+              params=params)
+
+
+mod$predict(data_file='examples/data/bcp_test_data.csv',
+              label_name='Class',
+              sample_id = 'Sample code number',
+              )
+
+
+
+# print the test data results table
+mod$results(include_label = T) %>% head() %>% print()
+
+# metrics on the test set predictions
+mod$classification_metrics() %>% print()
+
+# and get the importance of features in each ensemble member
+mod$importance() %>% print()
+
+# plot the ROC curves for each class
+## IF THE ROC IS UPSIDE DOWN, SET FLIP=T
+ensemble_rocs(mod, flip=F) # uses the last fold trained.
+
